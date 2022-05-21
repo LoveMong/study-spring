b97385a9928f3799e7d4fc3fd313817df5d83ef3
LeeMong <izpzgi2019@gmail.com>
2022-05-21 14:36:51 +0900

    데이터 변환, 검증

--- ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
+++ ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
@@ -1,18 +1,25 @@
 package com.fastcampus.ch2;
 
 import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
 
 import javax.annotation.PostConstruct;
+import javax.xml.crypto.Data;
 
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/register")
 public class RegisterController {
 
 //	GET요청이면(POST X) servlet-context.xml 에서 view-controller로 jsp 매핑 가능
@@ -23,9 +30,25 @@ public class RegisterController {
 //		return "registerForm";
 //	}
 	
+	@InitBinder
+	public void toDate(WebDataBinder binder) {
+//		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+//		binder.registerCustomEditor(Data.class,	new CustomDateEditor(df, false));
+		binder.registerCustomEditor(String[].class,	new StringArrayPropertyEditor("#"));
+	}
+	
+	@GetMapping("/add")
+	public String add() {
+		return "registerForm";		
+	}
+
+	
 //	@RequestMapping(value = "/register/save", method = RequestMethod.POST)
-	@PostMapping("/register/save") // 4.3버전 이후 부터 사용 가능
-	public String save(User user, Model model) throws Exception {
+	@PostMapping("/save") // 4.3버전 이후 부터 사용 가능
+	public String save(User user, BindingResult result, Model model) throws Exception {
+		
+		System.out.println("user : " + user);
+		
 //		1. 유효성 검사
 		
 		if(!isValid(user)) {
